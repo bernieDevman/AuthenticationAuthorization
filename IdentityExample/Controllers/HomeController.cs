f478cb4ac7a25d0ffd3b360ad78ed094697fc266
Bernardo Ramon <bernardo.gutierrez@finning.com>
2021-03-14 16:14:40 -0600

    initial commit

---
+++ IdentityExample/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string userName, string password)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if (user is not null)
+            {
+                var signInResult =  await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            var user = new IdentityUser()
+            {
+                UserName = username,
+                Email = string.Empty
+            };
+            
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+               //generation Email token
+            }
+
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Server/Crontrollers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Server.Crontrollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
